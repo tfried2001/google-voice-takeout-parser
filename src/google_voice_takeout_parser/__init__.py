776064e12bddf90fc9b249e3dd395c7e3eb271b0
tfried2001 <tfried_2001@yahoo.com>
2025-10-04 09:06:58 -0400

    Broke out specific imports from main parser

--- src/google_voice_takeout_parser/__init__.py
+++ src/google_voice_takeout_parser/__init__.py
@@ -1,2 +1,12 @@
 from ._version import __version__ as __version__  # noqa: F401
-from .google_voice_takeout_parser import * # noqa: F401
+from .google_voice_takeout_parser import (
+    parse_file,
+    parse_str,
+    process_directory,
+    write_to_csv,
+)
+
+__all__ = [
+    "parse_file", "parse_str", "process_directory", "write_to_csv",
+    "__version__"
+]
